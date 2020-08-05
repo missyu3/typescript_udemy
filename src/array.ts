881a7858c4cdfbf90c6f9710fee414fb30e5f12e
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-08-05 14:43:53 +0900

    Add array

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+let doubleArray: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let array: (string | number | boolean)[] = [1, false, 'Japan'];
+
+console.log(numbers);
+console.log(numbers2);
+console.log(doubleArray);
+console.log(array);
