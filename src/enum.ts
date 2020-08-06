cf7c67f7077536844a51628a9de810daf6b43084
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-08-06 14:04:48 +0900

    Add enum

---
+++ src/enum.ts
@@ -0,0 +1,32 @@
+export {};
+
+enum MONTHS {
+  January = 1,
+  February,
+  Match,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(MONTHS.January);
+console.log(MONTHS.February);
+console.log(MONTHS.December);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+}
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+}
+
+console.log(COLORS.RED);
+console.log(COLORS.YELLOW);
